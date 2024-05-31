9730cc96d5e2573efb6a37cfb2e66dd67be5e013
Clement-Sorin <clement.sorin.141@gmail.com>
2024-05-31 15:05:42 +0200

    footer : add elements + style

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,8 +1,11 @@
+import logo from '../../assets/logo-white.png'
+
 function Footer() {
     return (
-        <div className='Footer'>
-          <h1>Footer</h1>
-        </div>
+        <footer className='footer'>
+          <img src={logo} alt='logo de kasa' className='footer__logo' />
+          <p className='footer__text'>© 2020 Kasa. All right reserved</p>
+        </footer>
     )
 }
 
