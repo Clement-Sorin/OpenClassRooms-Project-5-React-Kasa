0572140993d71541b4a3bc4f9b2174e062ebc686
Clement-Sorin <clement.sorin.141@gmail.com>
2024-05-31 16:19:18 +0200

    home : add section 1 title & backgound image

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -1,8 +1,14 @@
+import backgound_img from '../../assets/coast-background.webp'
+import '../../sass/pages/Home.scss'
+
 function Home() {
   return (
-    <>
-      <h1>Coucou</h1>
-    </>
+    <main className="main">
+      <div className="section1">
+        <img src={backgound_img} className='section1__background' />
+        <h1 className="section1__title">Chez vous, partout et ailleurs</h1>
+      </div>
+    </main>
   );
 }
 
