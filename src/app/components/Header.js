21ffcbff75ae7dea21baad116ba588ae39d47559
Clement-Sorin <clement.sorin.141@gmail.com>
2024-05-31 11:58:24 +0200

    header: add elements and style

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,8 +1,15 @@
+import { NavLink } from 'react-router-dom'
+import logo from '../../assets/logo.png'
+
 function Header() {
     return (
-        <div className='header'>
-          <h1>Header</h1>
-        </div>
+        <header className='header'>
+          <img src={logo} alt='logo de kasa' className='header__logo' />
+          <nav className='navbar'>
+            <NavLink to={'/'} className={'navbar__link'} aria-label={`lien vers la page d'accueil de kasa`} >Accueil</NavLink>
+            <NavLink to={'/a-propos'} className={'navbar__link'} aria-label={`lien vers la page à propos de kasa`}>A propos</NavLink>
+          </nav>
+        </header>
     )
 }
 
