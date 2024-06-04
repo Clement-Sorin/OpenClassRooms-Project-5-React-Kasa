e0ce70ab637c533fbbad6a12bd47af6d9742efeb
Clément Sorin <clement.sorin.141@gmail.com>
2024-06-04 14:52:38 +0200

    Error404 : create page, styling element for desktop and mobile display

--- src/app/pages/Error404.js
+++ src/app/pages/Error404.js
@@ -1,7 +1,11 @@
-function PageError404() {
-    return <div>
-        <h1>Erreur 404</h1>
+import "../../sass/pages/Error404.scss"
+
+function Error404() {
+    return <div className="main-wrap">
+        <h1 className="main-wrap__title1">404</h1>
+        <h2 className="main-wrap__title2">Oups! La page que vous demandez n'existe pas.</h2>
+        <a href="/" aria-label="retour à la page d'accueil" className="main-wrap__link">Retourner sur la page d'accueil</a>
     </div>
 }
 
-export default PageError404
+export default Error404
