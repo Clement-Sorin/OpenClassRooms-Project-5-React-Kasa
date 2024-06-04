29aec9d8f220fb38e6d95eecd55a3b361e3292ea
Clément Sorin <clement.sorin.141@gmail.com>
2024-06-04 12:29:04 +0200

    about : animation on dropdown menu

--- src/app/components/Collapse.js
+++ src/app/components/Collapse.js
@@ -1,13 +1,30 @@
+import { useState } from "react"
 import "../../sass/components/Collapse.scss"
 import arrow from "../../assets/arrow_back_ios-24px 2.png"
 
 function Collapse({title, content}) {
+    const [isDropdownOpen, setDropdownOpen] = useState(false)
+    function toggleDropdown() {
+        setDropdownOpen(!isDropdownOpen)
+    }
+
+    let collapseContentClass = "collapse-content"
+    let collapseArrowClass = "collapse-bar__arrow"
+    if (isDropdownOpen) {
+        collapseContentClass = " collapse-content--open"
+        collapseArrowClass += " collapse-bar__arrow--open"
+    }
+
     return <div>
-        <div className="collapse-bar">
+        <div className="collapse-bar" onClick={toggleDropdown}>
             <h2 className="collapse-bar__title">{title}</h2>
-            <img className="collapse-bar__arrow" src={arrow} />
+            <img 
+                className= {collapseArrowClass}
+                src={arrow} 
+                alt="flèche pour ouvrir ou fermer le contenu du texte" 
+            />
         </div>
-        <div className="collapse-content">
+        <div className={collapseContentClass}>
             <p className="collapse-content__text">{content}</p>
         </div>
     </div>
