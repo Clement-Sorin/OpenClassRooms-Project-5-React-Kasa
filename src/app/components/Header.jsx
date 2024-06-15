9e0a6d79d44ef4883fc7dc5db6d93e3ce9a5c034
Clément Sorin <clement.sorin.141@gmail.com>
2024-06-15 19:46:28 +0200

    Navbar : add a border to selected link "Accueil" & "A propos"

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,23 +1,47 @@
 import { NavLink, Link } from "react-router-dom"
+import { useState } from "react"
 import logo from "../../assets/logo-orange.png"
 
 function Header() {
+    const [activePage, setActivePage] = useState("")
+    let classLinkHome = "navbar__link"
+    let classLinkAbout = "navbar__link"
+
+    switch (activePage) {
+        case "home":
+            classLinkHome += " active"
+            break
+        case "about":
+            classLinkAbout += " active"
+            break
+        default:
+            classLinkAbout += ""
+            classLinkHome += ""
+    }
+
     return (
         <header className="header">
             <Link to={"/"}>
-                <img src={logo} alt="logo de kasa" className="header__logo" />
+                <img
+                    src={logo}
+                    onClick={() => setActivePage("home")}
+                    alt="logo de kasa"
+                    className="header__logo"
+                />
             </Link>
             <nav className="navbar">
                 <NavLink
                     to={"/"}
-                    className={"navbar__link"}
+                    onClick={() => setActivePage("home")}
+                    className={classLinkHome}
                     aria-label={`lien vers la page d'accueil de kasa`}
                 >
                     Accueil
                 </NavLink>
                 <NavLink
                     to={"/a-propos"}
-                    className={"navbar__link"}
+                    onClick={() => setActivePage("about")}
+                    className={classLinkAbout}
                     aria-label={`lien vers la page à propos de kasa`}
                 >
                     A propos
